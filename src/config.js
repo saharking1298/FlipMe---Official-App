c032622cc94fe4767e4d616408815208dfa7a43c
saharking1298 <saharlan500@gmail.com>
2023-11-06 10:59:22 +0200

    Fixed minor dev mode bug

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     baseURL: dev ? '' : '/projects/flip-me',
-    publicPath: dev ? './' : '/flip-me'
+    publicPath: dev ? '/' : '/flip-me'
 };
